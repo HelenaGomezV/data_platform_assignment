d6b321cbd0cfcabdd8e9d3fa77d06ecf457d8726
HelenaGomezV <helena.villegas90@gmail.com>
2026-09-08 17:07:50 +0200

    feat: start service user module

---
+++ exercise-2/modules/service_user/variables.tf
@@ -0,0 +1,9 @@
+variable "service_name" {
+  description = "Name of the Snowflake service user"
+  type        = string
+}
+
+variable "role_name" {
+  description = "Snowflake role granted to the service user"
+  type        = string
+}
